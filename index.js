2b54aa3c4edd6a5255d05c0f8ddc6de87da1ab55
Ondicho <albert.ondicho@moringaschool.com>
2022-05-31 18:07:22 +0300

    commit tree 0ee1fe69126257b4a089c70614e4330e04e34943

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers= function (array){
+    return [array[0],array[1]]
+}
+
+const returnLastTwoDrivers= function (array){
+    let length=array.length
+    return [array[length-2],array[length-1]]
+}
+
+const selectingDrivers=[returnFirstTwoDrivers,returnLastTwoDrivers]
+
+
+const createFareMultiplier=function (int){
+    return function (fare){
+        return int*fare
+    }
+}
+
+
+const fareDoubler= function (int){
+    return createFareMultiplier(int)*2
+}
+
+const fareTripler=function (int){
+    return createFareMultiplier(int)
+}
+
+const selectDifferentDrivers= function(array,name){
+    if (name===returnFirstTwoDrivers){
+        return returnFirstTwoDrivers(array)
+    } else {
+        return returnLastTwoDrivers(array)
+    }
+}
